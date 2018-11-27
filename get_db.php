49ec3e000998841fbed81151d066f27664657c0a
MasterJuneLeung <44095041+MasterJuneLeung@users.noreply.github.com>
2018-11-27 08:29:04 -0400

    Delete get_db.php

--- get_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require("constants.php");
-try{
- $db = new PDO("mysql:host=" . $host . ";dbname=" . $database , $username, $password);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); }
-   catch(PDOException $e){
-    echo $e->getMessage(); }
-?>
